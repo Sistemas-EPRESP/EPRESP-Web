4ef4cb32c44e54fbb787f4ca3f3cbf58153ae8f5
Adriel-M-A <adriel.dev97@gmail.com>
2025-02-10 12:36:24 -0300

    Agregar enrutamiento básico y componente Home a la aplicación

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,10 @@
-<div>Home</div>
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Bienvenido a la Página Principal</h1>
+      <p>Esta es la página de inicio de tu aplicación React.</p>
+    </div>
+  );
+};
+
+export default Home;
