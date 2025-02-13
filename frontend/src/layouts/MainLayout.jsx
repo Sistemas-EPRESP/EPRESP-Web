0b69c74857c0affb8393a1eca76dea98be1a56ed
Adriel-M-A <adriel.dev97@gmail.com>
2025-02-13 09:51:31 -0300

    Agregar MainLayout para envolver el enrutador de la aplicación

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+const MainLayout = ({ children }) => {
+  return (
+    <div>
+      {/* <Header /> */}
+      <main>{children}</main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default MainLayout;
