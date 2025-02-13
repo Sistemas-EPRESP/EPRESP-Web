2fdac4cde779cf269561728678e145c8d1e92836
Adriel-M-A <adriel.dev97@gmail.com>
2025-02-13 09:51:50 -0300

    Actualizar rutas en AppRouter para mejorar la estructura y accesibilidad de formularios

--- frontend/src/routers/AppRouter.jsx
+++ frontend/src/routers/AppRouter.jsx
@@ -1,19 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import FormularioRendicion from '../pages/FormularioRendicion';
-import LogIn from '../pages/LogIn';
-import ProtectedRoute from './ProtectedRoute';
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import FormularioRendicion from "../pages/FormularioRendicion";
+import LogIn from "../pages/LogIn";
+import ProtectedRoute from "./ProtectedRoute";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LogIn />} />
-      <Route path="/formulario_rendicion" element={
-        <ProtectedRoute>
-          <FormularioRendicion />
-        </ProtectedRoute>
-      } />
+      <Route
+        path="formulario/formulario_rendicion"
+        element={
+          <ProtectedRoute>
+            <FormularioRendicion />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
