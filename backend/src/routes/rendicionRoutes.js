52b45f3ed454dbeb694cbd1712bddcd401309e95
cachi1997 <cachigaliano1997@gmail.com>
2025-02-26 11:00:12 -0300

    Agregar funcionalidades para modificar y aprobar formularios de rendición; actualizar rutas y servicios correspondientes.

--- backend/src/routes/rendicionRoutes.js
+++ backend/src/routes/rendicionRoutes.js
@@ -1,14 +1,29 @@
 const express = require('express');
-const { verificarToken } = require('../middlewares/authMiddlewares');
+const {
+  verificarToken,
+  verificarAdmin,
+  verificarCooperativa,
+} = require('../middlewares/authMiddlewares');
 const rendicionController = require('../controllers/rendicionController');
 const router = express.Router();
 
 router.post(
   '/formulario-rendicion/:id',
-  //verificarToken,
+  //verificarCooperativa,
   rendicionController.createFormularioRendicion,
 );
 
+router.put(
+  '/modificar-rendicion/:id',
+  //verificarCooperativa,
+  rendicionController.updateFormularioRendicion,
+);
+
 router.get('/obtener-rendicion/:id', rendicionController.obtenerRendicion);
+router.post(
+  '/aprobar-rendicion/:id',
+  //verificarAdmin,
+  rendicionController.aprobarRendicion,
+);
 
 module.exports = router;
