1885ad3b72fc357b3f60c8e36a4d516d51cab3eb
Adriel-M-A <adriel.dev97@gmail.com>
2025-02-27 10:30:40 -0300

    Agregar funciones para obtener el mes anterior y su nombre en utils de fecha

--- frontend/src/utils/dateUtils.js
+++ frontend/src/utils/dateUtils.js
@@ -17,3 +17,15 @@ export const getNombreMes = (mesNumero) => {
   // Restamos 1 ya que el array comienza en índice 0
   return monthNames[mesNumero - 1] || "";
 };
+
+export const getMesAnterior = (mes) => {
+  // Convertimos el mes a número en caso de que venga como string
+  const mesNumero = parseInt(mes, 10);
+  // Si es enero (1), el mes anterior es diciembre (12)
+  return mesNumero === 1 ? 12 : mesNumero - 1;
+};
+
+export const getNombreMesAnterior = (mes) => {
+  const mesAnteriorNumero = getMesAnterior(mes);
+  return getNombreMes(mesAnteriorNumero);
+};
