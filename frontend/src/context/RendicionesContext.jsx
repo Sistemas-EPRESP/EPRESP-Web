bc83ae4ff572cb1e2cb8f516a1e21f6f0c8c51eb
Adriel-M-A <adriel.dev97@gmail.com>
2025-03-27 12:28:56 -0300

    Eliminar logs de consola innecesarios en el contexto de Rendiciones

--- frontend/src/context/RendicionesContext.jsx
+++ frontend/src/context/RendicionesContext.jsx
@@ -1,14 +1,10 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState } from "react";
 
 const RendicionesContext = createContext();
 
 export const RendicionesProvider = ({ children }) => {
   const [rendiciones, setRendiciones] = useState([]);
-
-  useEffect(() => {
-    console.log("Rendiciones en el contexto:", rendiciones);
-  }, [rendiciones]);
 
   return <RendicionesContext.Provider value={{ rendiciones, setRendiciones }}>{children}</RendicionesContext.Provider>;
 };
